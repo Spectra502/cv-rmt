db223321b2ea595f802ff03c986d5efbb497bb05
Spectra <spectra502@gmail.com>
2024-04-07 21:29:42 +0200

    App.jsx actualized

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { BrowserRouter } from 'react-router-dom';
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
+
+
 const App = () => {
   return (
-    <div>
-      3D Portfolio
-    </div>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar/>
+          <Hero/>
+          <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+        <div className='relative z-0'>
+          <Contact></Contact>
+          <StarsCanvas></StarsCanvas>
+        </div> 
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 export default App
